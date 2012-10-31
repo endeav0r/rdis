c06a5421485c8f645fae6cbd8a1b4aef0374f52a
endeavor <endeavor@rainbowsandpwnies.com>
2012-10-31 12:17:16 -0500

    adding uint64 type to lua scripting

--- src/script/rdis_lua.c
+++ src/script/rdis_lua.c
@@ -6,9 +6,24 @@
 
 #include <stdlib.h>
 
+static const struct luaL_Reg rl_uint64_lib_m [] = {
+    {"__add", rl_uint64_add},
+    {"__sub", rl_uint64_sub},
+    {"__mul", rl_uint64_mul},
+    {"__div", rl_uint64_div},
+    {"__mod", rl_uint64_mod},
+    {"__eq",  rl_uint64_eq},
+    {"__lt",  rl_uint64_lt},
+    {"__le",  rl_uint64_le},
+    {"__tostring", rl_uint64_tostring},
+    {"string", rl_uint64_tostring},
+    {NULL, NULL}
+};
+
+
 static const struct luaL_Reg rl_rdis_lib_f [] = {
     {"console",   rl_rdis_console},
-    {"functions",   rl_rdis_functions},
+    {"functions", rl_rdis_functions},
     {NULL, NULL}
 };
 
@@ -21,6 +36,14 @@ struct _rdis_lua * rdis_lua_create (struct _rdis * rdis)
     rdis_lua->rdis = rdis;
     rdis_lua->L = lua_open();
     luaL_openlibs(rdis_lua->L);
+
+    luaL_newmetatable(rdis_lua->L, "rdis.uint64");
+    luaL_register(rdis_lua->L, NULL, rl_uint64_lib_m);
+    lua_pushstring(rdis_lua->L, "__index");
+    lua_pushvalue(rdis_lua->L, -2);
+    lua_settable(rdis_lua->L, -3);
+
+    lua_register(rdis_lua->L, "uint64", rl_uint64);
 
     lua_pushlightuserdata(rdis_lua->L, rdis_lua);
     lua_setfield(rdis_lua->L, LUA_REGISTRYINDEX, "rdis_lua");
@@ -48,8 +71,180 @@ int rdis_lua_execute (struct _rdis_lua * rdis_lua, const char * string)
         return -1;
     }
     return 0;
-
 }
+
+
+/****************
+* rl_uint64
+****************/
+
+int rl_uint64 (lua_State * L)
+{
+    uint64_t uint64_value = 0;
+
+    if (lua_isnumber(L, -1)) {
+        uint64_value = luaL_checkinteger(L, -1);
+        lua_pop(L, -1);
+    }
+    else if (lua_isstring(L, -1)) {
+        const char * s = luaL_checkstring(L, -1);
+        uint64_value = strtoull(s, NULL, 0);
+        lua_pop(L, -1);
+    }
+    else {
+        luaL_error(L, "uint64 must be called with a number or string");
+    }
+
+    printf("a\n");
+    return rl_uint64_push(L, uint64_value);
+}
+
+
+int rl_uint64_push (lua_State * L, uint64_t value)
+{
+    uint64_t * value_ptr = lua_newuserdata(L, sizeof(uint64_t));
+    luaL_getmetatable(L, "rdis.uint64");
+    lua_setmetatable(L, -2);
+
+    *value_ptr = value;
+
+    printf("push\n");
+
+    return 1;
+}
+
+
+uint64_t rl_check_uint64 (lua_State * L, int position)
+{
+    void * data = luaL_checkudata(L, position, "rdis.uint64");
+    luaL_argcheck(L, data != NULL, position, "expected uint64");
+    return *((uint64_t *) data);
+}
+
+
+int rl_uint64_add (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+    
+    rl_uint64_push(L, lhs + rhs);
+
+    return 1;
+}
+
+
+int rl_uint64_sub (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+    
+    rl_uint64_push(L, lhs - rhs);
+
+    return 1;
+}
+
+
+int rl_uint64_mul (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+    
+    rl_uint64_push(L, lhs * rhs);
+
+    return 1;
+}
+
+
+int rl_uint64_div (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+    
+    rl_uint64_push(L, lhs / rhs);
+
+    return 1;
+}
+
+
+int rl_uint64_mod (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+    
+    rl_uint64_push(L, lhs % rhs);
+
+    return 1;
+}
+
+
+int rl_uint64_eq  (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+
+    if (lhs == rhs)
+        lua_pushboolean(L, 1);
+    else
+        lua_pushboolean(L, 0);
+
+    return 1;
+}
+
+
+int rl_uint64_lt  (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+
+    if (lhs < rhs)
+        lua_pushboolean(L, 1);
+    else
+        lua_pushboolean(L, 0);
+
+    return 1;
+}
+
+
+int rl_uint64_le  (lua_State * L)
+{
+    uint64_t lhs = rl_check_uint64(L, -1);
+    uint64_t rhs = rl_check_uint64(L, -2);
+    lua_pop(L, 2);
+
+    if (lhs <= rhs)
+        lua_pushboolean(L, 1);
+    else
+        lua_pushboolean(L, 0);
+
+    return 1;
+}
+
+
+int rl_uint64_tostring (lua_State * L)
+{
+    char tmp[64];
+
+    uint64_t value = rl_check_uint64(L, -1);
+    lua_pop(L, -1);
+
+    snprintf(tmp, 64, "0x%llx", (unsigned long long) value);
+
+    lua_pushstring(L, tmp);
+
+    return 1;
+}
+
+
+/****************
+* rl_rdis
+****************/
 
 
 struct _rdis_lua * rl_get_rdis_lua (lua_State * L)
@@ -86,9 +281,7 @@ int rl_rdis_functions (lua_State * L)
          it = tree_it_next(it)) {
         struct _index * index = tree_it_data(it);
 
-        char tmp[32];
-        snprintf(tmp, 32, "0x%04llx", (unsigned long long) index->index);
-        lua_pushstring(L, tmp);
+        rl_uint64_push(L, index->index);
 
         struct _label * label = map_fetch(rdis_lua->rdis->labels, index->index);
         lua_pushstring(L, label->text);
--- src/script/rdis_lua.h
+++ src/script/rdis_lua.h
@@ -18,6 +18,20 @@ void               rdis_lua_delete  (struct _rdis_lua * rdis_lua);
 int                rdis_lua_execute (struct _rdis_lua * rdis_lua,
 									 const char * string);
 
+int      rl_uint64_push  (lua_State * L, uint64_t value);
+uint64_t rl_check_uint64 (lua_State * L, int position);
+int rl_uint64          (lua_State *L);
+int rl_uint64_add      (lua_State *L);
+int rl_uint64_sub      (lua_State *L);
+int rl_uint64_mul      (lua_State *L);
+int rl_uint64_div      (lua_State *L);
+int rl_uint64_mod      (lua_State *L);
+int rl_uint64_eq       (lua_State *L);
+int rl_uint64_lt       (lua_State *L);
+int rl_uint64_le       (lua_State *L);
+int rl_uint64_tostring (lua_State *L);
+
+
 int rl_rdis_console   (lua_State * L);
 int rl_rdis_functions (lua_State * L);
 
