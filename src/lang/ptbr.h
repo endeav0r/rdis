2aa3676734f265bb59706a4fa5f25fb16cabc811
TDKPS <TDKPS@tdkps.mil>
2012-12-30 10:30:48 -0200

    Added ptbr language.

---
+++ src/lang/ptbr.h
@@ -0,0 +1,36 @@
+#ifndef lang_HEADER
+#define lang_HEADER
+
+// GUI
+#define LANG_ACTIVATE      "Ativar"
+#define LANG_ADDRESS       "Endereço da Memória"
+#define LANG_ALL           "Todos"
+#define LANG_BYTES         "Bytes"
+#define LANG_CALLGRAPH     "Chamar Gráfico"
+#define LANG_COMMENT       "Commentar"
+#define LANG_DESCRIPTION   "Descrição"
+#define LANG_FUNCTIONS     "Funções"
+#define LANG_HEXVIEW       "Ver Hex"
+#define LANG_LOADEXEC      "Carregar Executável"
+#define LANG_LOADEXECFILE  "Carregar arquivo executável"
+#define LANG_MARKREACHABLE "Marcar função como alcançável"
+#define LANG_MENUFILE      "Arquivo"
+#define LANG_NAME          "Nome"
+#define LANG_OPENRDIS      "Abrir .rdis"
+#define LANG_OPENRDISFILE  "Abrir arquivo Rdis "
+#define LANG_REACHABLE     "Alcançáveis"
+#define LANG_REFERENCES    "Referências"
+#define LANG_REFERENCERS   "Referenciados"
+#define LANG_REMOVEFUNC    "Remover Função"
+#define LANG_SAVERDIS      "Salvar .rdis"
+#define LANG_SAVERDISFILE  "Safe Rdis File"
+#define LANG_UNREACHABLE   "Funções Não Alcançáveis"
+#define LANG_USERFUNCTION  "Funções definidas pelo usuário"
+
+// RDIS
+#define LANG_MEMORYLOADED "Memória carregada"
+#define LANG_FUNCSLOADED  "Funções carregadas"
+#define LANG_GRAPHLOADED  "Gráfico carregado"
+#define LANG_LABELSLOADED "Etiquetas carregadas"
+
+#endif
